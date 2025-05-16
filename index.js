3620d27a69032a7a29537b6ce3b31ed1ab08c716
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-16 09:08:05 -0700

    mongo db installed and simple data recieve

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const cors = require ('cors')
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const port =process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 // middleware 
 app.use(cors())
@@ -18,20 +18,43 @@ const uri = "mongodb+srv://module55server:QQ1ar7mV16tPAsNr@cluster0.eztfylz.mong
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
 
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-app.get('/',(req,res) =>{
+        //  data sendin on server 
+        app.post('/users',(req,res) => {
+            console.log(req.body)
+        })
+
+
+        // connection check kore commment kore dilam 
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        //   finally function clear kore dilam mongo theke ene.
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
     res.send('server is running')
 })
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`app is running on ${port}`)
 })
